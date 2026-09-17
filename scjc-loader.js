a4bf201b7f84226792d44ab4ef15aa84ec622b74
petomalik6-duda <petomalik6@gmail.com>
2026-09-17 20:16:12 +0200

    Remove temporary SCJC deployment file

--- scjc-loader.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const fs = require('node:fs');
-const zlib = require('node:zlib');
-const path = require('node:path');
-const packed = fs.readFileSync(path.join(__dirname, 'scjc-server.js.gz.b64'), 'utf8').trim();
-const source = zlib.gunzipSync(Buffer.from(packed, 'base64')).toString('utf8');
-const run = new Function('require', 'module', 'exports', '__filename', '__dirname', source);
-run(require, module, exports, path.join(__dirname, 'server.js'), __dirname);
